dbb7a9ca9029e104c094d73015d6c0965e31f167
MouzinhoA <124109065+MouzinhoA@users.noreply.github.com>
2023-07-15 14:44:22 -0300

    Update and rename BotãoTelamenorlogin.js to tela.js

---
+++ tela.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={[styles.container, styles.shadowProp]}>
+      </View>
+
+  );
+}
+
+
