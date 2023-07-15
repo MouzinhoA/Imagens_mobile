1e7bb77ea774d4913fd13c6b9fbebb633a61c097
MouzinhoA <124109065+MouzinhoA@users.noreply.github.com>
2023-07-15 15:06:34 -0300

    Update tela.js

--- tela.js
+++ tela.js
@@ -7,5 +7,57 @@ export default function App() {
 
   );
 }
+//box maior
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#B2AED8",
+    borderRadius: 17,
+    alignSelf: "center",
+    width: 337,
+    height: 291,
+   
+  
+  },
+  shadowProp: {
+    elevation: 120, 
+    shadowColor: "black",    
+  },
+})
 
+//box menor 
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#C9C7E8",
+    borderRadius: 15,
+    alignSelf: "center",
+    width: 276,
+    height: 174,
+   
+  
+  },
+  shadowProp: {
+    elevation: 20, 
+    shadowColor: "darkorchid",    
+    shadowOpacity: 0.8
+  },
+})
+
+//box rodapé
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#B2AED8",
+    borderRadius: 49,
+    alignSelf: "center",
+    width: 353,
+    height: 67,
+   
+  
+  },
+  shadowProp: {
+    elevation: 20, 
+    shadowColor: "black",    
+    shadowOpacity: 0.8
+  },
+})
